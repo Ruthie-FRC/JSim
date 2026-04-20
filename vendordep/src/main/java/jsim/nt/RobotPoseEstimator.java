70271abf924e77f35d02aaeb636c86780a6779bc
Ruthie <s2169121@online.houstonisd.org>
2026-04-19 21:32:38 -0500

    Add RobotPoseEstimator class for differential drive robot pose estimation

---
+++ vendordep/src/main/java/jsim/nt/RobotPoseEstimator.java
@@ -0,0 +1,44 @@
+package jsim.nt;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+
+/**
+ * Simple pose estimator for a differential drive robot using encoder and gyro data.
+ * Extend for more advanced estimation or other drive types.
+ */
+public class RobotPoseEstimator {
+    private final DifferentialDriveOdometry odometry;
+
+    /**
+     * @param initialPose Initial robot pose
+     * @param initialGyroRadians Initial gyro angle (radians, CCW+)
+     */
+    public RobotPoseEstimator(Pose2d initialPose, double initialGyroRadians) {
+        this.odometry = new DifferentialDriveOdometry(
+            new Rotation2d(initialGyroRadians),
+            0.0, // initial left encoder distance
+            0.0, // initial right encoder distance
+            initialPose
+        );
+    }
+
+    /**
+     * Updates the pose estimator with new sensor readings.
+     * @param leftMeters Total left encoder distance (meters)
+     * @param rightMeters Total right encoder distance (meters)
+     * @param gyroRadians Current gyro angle (radians, CCW+)
+     * @return Estimated robot pose
+     */
+    public Pose2d update(double leftMeters, double rightMeters, double gyroRadians) {
+        return odometry.update(new Rotation2d(gyroRadians), leftMeters, rightMeters);
+    }
+
+    /**
+     * Gets the current estimated pose.
+     */
+    public Pose2d getEstimatedPose() {
+        return odometry.getPoseMeters();
+    }
+}
