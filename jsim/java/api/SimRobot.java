69d8344be1a0814208667feae7beb1a8213796d6
Ruthie <s2169121@online.houstonisd.org>
2026-04-30 11:24:04 -0500

    Refactor SimRobot constructor to include FieldState reference

--- jsim/java/api/SimRobot.java
+++ jsim/java/api/SimRobot.java
@@ -14,15 +14,17 @@ import core.StateManager;
 public final class SimRobot {
     private final RobotID robotID;
     private final StateManager stateManager;
+    private final FieldState<RobotState> stateManagerRef;
 
-    private SimRobot(RobotID robotID, StateManager stateManager) {
+    private SimRobot(RobotID robotID, StateManager stateManager, FieldState<RobotState> stateManagerRef) {
         this.robotID = robotID;
         this.stateManager = stateManager;
+        this.stateManagerRef = stateManagerRef;
     }
 
     public static SimRobot createRobot(Translation2d[] frameDimensions, StateManager stateManager, RobotID robotID) {
-        stateManager.initializeRobot(robotID, new Pose2d(0, 0, 0), frameDimensions);
-        return new SimRobot(robotID, stateManager);
+        FieldState<RobotState> ref = stateManager.initializeRobot(robotID, new Pose2d(0,0,0), frameDimensions);
+        return new SimRobot(robotID, stateManager, ref);
     }
 
     public Pose2d getPose() {
